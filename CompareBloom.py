9f104ce4694fee9431d1f11909efeb92defb3462
joshu <joshu@joshu-Inspiron-660.(none)>
2014-02-20 23:19:37 -0500

    Finished.

--- CompareBloom.py
+++ CompareBloom.py
@@ -1,22 +1,23 @@
-import subprocess,sys
-import time
+import subprocess,sys,optparse,time
 
-
-def main(argv):
+def main(hashStart,hashEnd,hashInc,
+	batchStart,batchEnd,batchInc):
 	#Vary the number of hash functions...
-	for numHash in range(1,10000,10):
+	for numHash in range(hashStart,hashEnd+hashInc,hashInc):
 		f=open("data/reg/time/numHash_"+str(numHash),"w+")
 		f.write("#Hash,BatchSize(Bytes),GPUTime,RegTime\n")
 		#Now, vary size (in bytes) of the batches being inserted.
-		for batchSize in range(10000000,1000000000,10000000):
+		for batchSize in range(batchStart,batchEnd+batchInc,batchInc):
 			#Remove any data that already exists in the directory.
 			subprocess.call(["rm","-rf",".txt"])
 			#Generate the batch data.	
 			subprocess.call(["bin/main.exe","--batchSize",str(batchSize),
-				"--generate","--silent"])
+				"--generate","--silent","--numBatches","1","--trueBatches","1",
+				"--falseBatches","1"])
 			#Setup the basic arguments.
 			basicArgs = ["--hashes",str(numHash),"--batchSize",str(batchSize),
-				"--silent","--size","100000000"]
+				"--silent","--size","100000000","--numBatches","1","--trueBatches","1"
+				,"--falseBatches","1"]
 			gpuArgs = ["bin/gpuMain.exe"]
 			gpuRun = []
 			gpuRun[len(gpuRun):] = gpuArgs
@@ -52,5 +53,32 @@ def main(argv):
 		f.close()				
 
 if __name__=="__main__":
-	main(sys.argv[0:len(sys.argv)])
-
+	hashStart = 1;
+	hashEnd = 11
+	hashInc = 2
+	batchStart = 10000
+	batchEnd = 60000
+	batchInc= 10000
+	parser = optparse.OptionParser()
+	parser.add_option("--hashStart")
+	parser.add_option("--hashEnd")
+	parser.add_option("--hashInc")
+	parser.add_option("--batchStart")
+	parser.add_option("--batchEnd")
+	parser.add_option("--batchInc")	
+	(options,args) = parser.parse_args()
+	if(options.hashStart is not None):
+		hashStart = int(options.hashStart)
+	if(options.hashEnd is not None):
+		hashEnd = int(options.hashEnd)
+	if(options.hashInc is not None):
+		hashInc = int(options.hashInc)	
+	if(options.batchStart is not None):
+		batchStart = int(options.batchStart)
+	if(options.batchEnd is not None):
+		batchEnd = int(options.batchEnd)
+	if(options.batchInc is not None):
+		batchInc = int(options.batchInc)	
+	main(hashStart = hashStart,hashEnd=hashEnd,hashInc=hashInc,
+		batchStart = batchStart,batchEnd = batchEnd,batchInc = batchInc)
+		
--- ComparePBF.py
+++ ComparePBF.py
@@ -1,13 +1,12 @@
-import subprocess,sys
-import time
+import subprocess,sys,time,optparse
 
-def main(args):	
+def main(hashStart,hashEnd,hashInc,batchStart,batchEnd,batchInc):	
 	#Firstly, vary the number of hash functions.
-	for numHash in range(1,10000,10):
+	for numHash in range(hashStart,hashEnd+hashInc,hashInc):
 		f = open("data/pbf/time/numHash_"+str(numHash),"w+")
 		f.write("#Hash,BatchSize(Bytes),PBFTime,RegTime\n")	
 		#Now, vary size (in bytes) of the batches being inserted. 
-		for batchSize in range(10000000,1000000000,10000000):
+		for batchSize in range(batchStart,batchEnd+batchInc,batchInc):
 			#Remove any data that already exists in the directory.
 			subprocess.call(["rm","-rf",".txt"])
 			#Generate the batch data.
@@ -22,8 +21,8 @@ def main(args):
 			pbfRun[len(pbfRun):] = basicArgs
 			#Add one more argument for the output of the PBF.
 			#So, the stats of each PBF can be compared.	
-			pbfRun[len(pbfRun):] = ["--pbfOutput",
-				"data/pbf/pbfResult/gpu_"+str(numHash)+"_"+str(batchSize)+".txt"]		
+			#pbfRun[len(pbfRun):] = ["--pbfOutput",
+			#	"data/pbf/pbfResult/gpu_"+str(numHash)+"_"+str(batchSize)+".txt"]		
 			startPbf = time.time()		
 			pbfProcess = subprocess.call(pbfRun)
 			endPbf = time.time()
@@ -31,8 +30,8 @@ def main(args):
 			regRun = []
 			regRun[len(regRun):] = regArgs
 			regRun[len(regRun):] = basicArgs
-			regRun[len(regRun):] = ["--pbfOutput",
-				"data/pbf/pbfResult/reg_"+str(numHash)+"_"+str(batchSize)+".txt"]
+			#regRun[len(regRun):] = ["--pbfOutput",
+			#	"data/pbf/pbfResult/reg_"+str(numHash)+"_"+str(batchSize)+".txt"]
 			startReg = time.time()
 			regProcess = subprocess.call(regRun)
 			endReg = time.time()
@@ -42,4 +41,31 @@ def main(args):
 			print "Update: Wrote hash: "+str(numHash)+" batchSize "+str(batchSize)
 		f.close()
 if __name__=="__main__":
-	main(sys.argv[1:len(sys.argv)])
+	hashStart = 1;
+	hashEnd = 11
+	hashInc = 2
+	batchStart = 10000
+	batchEnd = 60000
+	batchInc= 10000
+	parser = optparse.OptionParser()
+	parser.add_option("--hashStart")
+	parser.add_option("--hashEnd")
+	parser.add_option("--hashInc")
+	parser.add_option("--batchStart")
+	parser.add_option("--batchEnd")
+	parser.add_option("--batchInc")	
+	(options,args) = parser.parse_args()
+	if(options.hashStart is not None):
+		hashStart = int(options.hashStart)
+	if(options.hashEnd is not None):
+		hashEnd = int(options.hashEnd)
+	if(options.hashInc is not None):
+		hashInc = int(options.hashInc)	
+	if(options.batchStart is not None):
+		batchStart = int(options.batchStart)
+	if(options.batchEnd is not None):
+		batchEnd = int(options.batchEnd)
+	if(options.batchInc is not None):
+		batchInc = int(options.batchInc)	
+	main(hashStart = hashStart,hashEnd=hashEnd,hashInc=hashInc,
+		batchStart = batchStart,batchEnd = batchEnd,batchInc = batchInc)
